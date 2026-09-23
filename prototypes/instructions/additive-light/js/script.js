ff2a548112a49780decf10dbe59545a4e8db8c2b
ImBear001 <myrans.tyler@gmail.com>
2026-09-23 15:07:24 -0400

    canvas with HSB color mode, trying hue/saturation/brightness instead of RGB

--- prototypes/instructions/additive-light/js/script.js
+++ prototypes/instructions/additive-light/js/script.js
@@ -1,24 +1,30 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Additive Light
+ * Tyler Myrans
+ *
+ * An abstract piece about mixing light instead of paint.
+ *
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas and switches to HSB colour
+ */
 function setup() {
-
+    createCanvas(500, 500);
+    // Hue is 0-360 (degrees on the colour wheel),
+    // saturation and brightness are 0-100
+    colorMode(HSB, 360, 100, 100);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws the scene
+ */
 function draw() {
-
+    // Almost-black background so the light has something to glow against
+    background(0, 0, 5);
 }
