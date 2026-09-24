76fe4d6833cd41bc0e9460007e74bcff9661dbb1
ImBear001 <myrans.tyler@gmail.com>
2026-09-24 15:53:00 -0400

    Waterfall and river, white lines make the water look like it's moving even though nothing moves

--- prototypes/instructions/upside-down-catch/js/script.js
+++ prototypes/instructions/upside-down-catch/js/script.js
@@ -2,7 +2,7 @@
  * Upside Down Catch
  * Tyler Myrans
  *
- * The bear catches a salmon, but everything is flipped.
+ * The bear from the Cave catches a salmon, but everything is flipped.
  *
  * Uses:
  * p5.js
@@ -23,6 +23,7 @@ function setup() {
  */
 function draw() {
     drawBackground();
+    drawWaterfall();
 }
 
 /**
@@ -40,5 +41,43 @@ function drawBackground() {
     triangle(60, 200, 110, 70, 160, 200);
     triangle(340, 200, 390, 80, 440, 200);
     triangle(420, 200, 465, 100, 510, 200);
+    pop();
+}
+
+/**
+ * Draws the rock ledge, the waterfall pouring off it, and the river
+ */
+function drawWaterfall() {
+    // Rock ledge across the whole canvas
+    push();
+    noStroke();
+    fill(110, 105, 100);
+    rect(0, 190, 500, 40);
+    pop();
+
+    // Falling water
+    push();
+    noStroke();
+    fill(120, 180, 230);
+    rect(0, 225, 500, 115);
+    pop();
+
+    // White streaks so the water looks like it's falling
+    push();
+    stroke(235, 245, 255);
+    strokeWeight(3);
+    line(30, 235, 30, 320);
+    line(90, 240, 90, 300);
+    line(150, 230, 150, 330);
+    line(360, 235, 360, 315);
+    line(420, 240, 420, 330);
+    line(470, 230, 470, 300);
+    pop();
+
+    // The river at the bottom
+    push();
+    noStroke();
+    fill(60, 120, 190);
+    rect(0, 330, 500, 170);
     pop();
 }
