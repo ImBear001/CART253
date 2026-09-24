65546549f70689c4167c71edbd37640f89b54d72
ImBear001 <myrans.tyler@gmail.com>
2026-09-24 15:40:21 -0400

    sky and pine tress, the trees are triangles very simple

--- prototypes/instructions/upside-down-catch/js/script.js
+++ prototypes/instructions/upside-down-catch/js/script.js
@@ -1,24 +1,44 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Upside Down Catch
+ * Tyler Myrans
+ *
+ * The bear catches a salmon, but everything is flipped.
+ *
+ * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas
+ */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
+/**
+ * Draws the scene back to front
+ */
+function draw() {
+    drawBackground();
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Draws the sky and a row of pine trees on the horizon
+ */
+function drawBackground() {
+    // Pale sky
+    background(170, 210, 235);
 
+    // Pine trees are just dark green triangles
+    push();
+    noStroke();
+    fill(40, 90, 60);
+    triangle(0, 200, 40, 90, 80, 200);
+    triangle(60, 200, 110, 70, 160, 200);
+    triangle(340, 200, 390, 80, 440, 200);
+    triangle(420, 200, 465, 100, 510, 200);
+    pop();
 }
